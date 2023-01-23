16b8f77a1f6f96265c67c82d514dd5747935b726
KazGielen3 <k.l.gielen@student.tue.nl>
2023-01-23 20:24:25 +0100

    added fade in animation
    plus bike autonomy blinking

--- DGP-game/Assets/Scripts/SceneSwitcher.cs
+++ DGP-game/Assets/Scripts/SceneSwitcher.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+
     public void playGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
